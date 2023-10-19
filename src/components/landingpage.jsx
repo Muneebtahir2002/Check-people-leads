5a5220ab5acf10a8648e51478d168e4a6c26a582
Muneebtahir2002 <101181873+Muneebtahir2002@users.noreply.github.com>
2023-10-19 19:10:19 +0500

    Initial commit

---
+++ src/components/landingpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import IMG from "../images/Duck.svg"
+import Apbar from "../small components/appbar"
+const landingpage =()=>{
+return(
+    <div>
+        <Apbar/>
+    </div>
+)
+}
